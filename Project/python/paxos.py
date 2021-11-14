079682db3ab8e34ea340ac5985fed863130bb118
Michal Burgunder <michal_burgunder@yahoo.com>
2021-11-14 14:51:29 +0100

    comments for overview of requirements

--- Project/python/paxos.py
+++ Project/python/paxos.py
@@ -41,11 +41,43 @@ def parse_cfg(cfgpath):
 
 def acceptor(config, id):
   print '-> acceptor', id
-  state = {}
+  state = {
+    v_round: 0,
+    last_round_participated_in: 0,
+    v_value : None,
+  }
+
   r = mcast_receiver(config['acceptors'])
   s = mcast_sender()
   while True:
     msg = r.recv(2**16)
+    
+# v-round: comes from proposer
+# round: the last round the acceptor has participated in
+# v-value: The final value that needs be learned by the learners
+
+    # 1a
+    # receive c-round from proposer
+    #   
+
+    # 1b
+    # send the current v-value & v-round & c-round to proposer
+    # 
+
+    # 2a
+    # receive c-round, v-val from proposer
+    # REQUIREMENTS:
+    # cannot participate in round that is lower than the one it has already participated in
+    # if receive c-message that has lower round, sends abort
+    # if receive c-message that has a higher value, then it replaces the current -value with that c-message
+    # If "accept": assign c-value, c-round to v-val, v-round
+
+
+    # 2b
+    # at every received message, sends a message to the propsers on whether they "accepted" or "aborted" (v-round, v-value)
+    # 
+
+
     # fake acceptor! just forwards messages to the learner
     if id == 1:
       # print "acceptor: sending %s to learners" % (msg)
@@ -56,6 +88,27 @@ def proposer(config, id):
   print '-> proposer', id
   r = mcast_receiver(config['proposers'])
   s = mcast_sender()
+
+    # 1a
+    # send c-rnd to all acceptors
+
+    # 1b
+    # receive all round, v-round, v-value
+    # to move to around 2a, take the highest round number, and use that
+
+    # 2a
+    # REQUIREMENTS:
+    # If one message is abort (message loss), go back to 1a with new (higher) round number
+
+
+    # 2b
+    # receive v-round, v-value
+    # confirm that a majority of acceptors have accepted the proposed value
+    # If yes, send v-val to all learners
+    # If no, resend...? (?) CHECK, WE DONT KNOW
+
+  # sends the agreed upon value to the learners
+
   while True:
     msg = r.recv(2**16)
     # fake proposer! just forwards message to the acceptor
