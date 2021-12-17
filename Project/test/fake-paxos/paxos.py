8b8beb07ab916cab4baafbe90cc5a1470d046894
Aki Lin <amy881225@gmail.com>
2021-12-17 16:27:09 +0100

    paxos in test and fake-paxos

--- Project/test/fake-paxos/paxos.py
+++ Project/test/fake-paxos/paxos.py
@@ -145,41 +145,63 @@ def proposer(config, id):
   while True:
     # msg = r.recv(2**16)
     # 1a
-    msg_1a = {'stage':'1a'}
-    # randomly increase the c_rnd as initiate
-    pro_states['c_rnd'] = pro_states['c_rnd'] + random.randint(0,5)
-    msg_1a['c_rnd'] = pro_states['c_rnd']
-    s.sendto(msg_1a, config['acceptors'])
+    if id == 0 and propose_times>0 and not in_propose:
+      msg_1a = {'stage':'1a'}
+      # randomly increase the c_rnd as initiate
+      pro_states['c_rnd'] = pro_states['c_rnd'] + random.randint(0,5)
+      msg_1a['c_rnd'] = pro_states['c_rnd']
+      s.sendto(msg_1a, config['acceptors'])
+      time.sleep(1)
+      print("Send 1a message")
+      print(msg_1a)
+      propose_times = propose_times -1
+      in_propose = True
     # 1b receive 1b,rnd,v_rnd,v_val
-    #??? CHECK RECEIVE FORMAT
-    msg_1b_r = r.recv(2**16)
-    pro_states['v_rnd'].append(msg_1b_r['v_rnd'])
-    pro_states['v_val'].append(msg_1b_r['v_val'])
-    k = max(pro_states['v_rnd']) # need to check the format
-    k_index = pro_states['v_rnd'].index(k)
-    v = 'init'
-    if k == 0:
-      pro_states['c_val'] = v
-    else:
-      pro_states['c_val'] = pro_states['v_rnd'][k_index]
-    # 2a
-    msg_2a = {}
-    msg_2a['stage'] =  '2a'
-    msg_2a['c_rnd'] = pro_states['c_rnd']
-    msg_2a['c_val'] = pro_states['c_val']
-    s.sendto(msg_2a, config['acceptors'])
-    # 2b & decision
-    msg_2b_r = r.recv(2**16)
-    msg_dec = {}
-    msg_dec['stage'] = 'dec'
-    #??? CHECK RECEIVE FORMAT
-    pro_states['v_rnd'].append(msg_1b_r['v_rnd'])
-    pro_states['v_val'].append(msg_1b_r['v_val'])
-    if set(pro_states['v_rnd']) == set([pro_states['c_rnd']]):
-      msg_dec['v_val'] = pro_states['c_val']
-    else:
-      msg_dec['v_val'] = ''
-    s.sendto(msg_dec, config['learners'])
+    msg = r.recv(2**16)
+    if msg :
+      print(msg)
+      if msg['stage'] == '1b':
+        print("received 1b message")
+        pro_states['v_rnd'].append(msg['v_rnd'])
+        pro_states['v_val'].append(msg['v_val'])
+        k = max(pro_states['v_rnd']) # need to check the format
+        k_index = pro_states['v_rnd'].index(k)
+        v = 'init'
+        if k == 0:
+           pro_states['c_val'] = v
+        else:
+           pro_states['c_val'] = pro_states['v_val'][k_index]
+        # 2a
+        msg_2a = {}
+        msg_2a['stage'] =  '2a'
+        msg_2a['c_rnd'] = pro_states['c_rnd']
+        msg_2a['c_val'] = pro_states['c_val']
+        s.sendto(msg_2a, config['acceptors'])
+        time.sleep(1)
+        print("send 2a message")
+        print(msg_2a)
+      elif msg['stage'] == '2b'
+        # 2b & decision
+        msg_dec = {}
+        msg_dec['stage'] = 'dec'
+        pro_states['v_rnd'].append(msg['v_rnd'])
+        pro_states['v_val'].append(msg['v_val'])
+        if set(pro_states['v_rnd']) == set([pro_states['c_rnd']]):
+          msg_dec['v_val'] = pro_states['c_val']
+        else:
+          msg_dec['v_val'] = ''
+          s.sendto(msg_dec, config['learners'])
+          time.sleep(1)
+          print("send decision message")
+          print(msg_dec)
+          in_propose = False
+      else:
+        print("message received not 1b not 2b")
+        print("repropse 1a stage")
+        time.sleep(1)
+        in_propose = False
+        propose_times = propose_times + 1
+
 
 
 
